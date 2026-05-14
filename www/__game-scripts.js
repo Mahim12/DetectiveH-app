1d1f1b0e0ffdac6ed694d9784127cda118ce7672
Mahim Dhungel <mdrezark@gmail.com>
2026-05-14 18:43:13 -0400

    Updated __game-scripts.js to account for mobile touch inputs

--- www/__game-scripts.js
+++ www/__game-scripts.js
@@ -1,1 +1,96 @@
-var FollowCamera=pc.createScript("followCamera");FollowCamera.attributes.add("target",{type:"entity",title:"Target",description:"The Entity to follow"}),FollowCamera.attributes.add("distance",{type:"number",default:4,title:"Distance",description:"How far from the Entity should the follower be"}),FollowCamera.prototype.initialize=function(){this.pos=new pc.Vec3,this.offset=new pc.Vec3},FollowCamera.prototype.postUpdate=function(t){this.target&&(this.offset.set(.75,1,.75).scale(this.distance),this.pos.copy(this.target.getPosition()).add(this.offset),this.pos.lerp(this.entity.getPosition(),this.pos,.1),this.entity.setPosition(this.pos))};var Movement=pc.createScript("movement");Movement.attributes.add("speed",{type:"number",default:.1,min:.05,max:.5,precision:2,description:"Controls the movement speed"}),Movement.prototype.initialize=function(){this.force=new pc.Vec3,this.spawnPos=this.entity.getPosition().clone()},Movement.prototype.update=function(e){if(this.entity.getPosition().y<-1)return void this.teleport(this.spawnPos);const t=this.app.keyboard;let s=0,i=0;if((t.isPressed(pc.KEY_LEFT)||t.isPressed(pc.KEY_A))&&(s=-this.speed),(t.isPressed(pc.KEY_RIGHT)||t.isPressed(pc.KEY_D))&&(s+=this.speed),(t.isPressed(pc.KEY_UP)||t.isPressed(pc.KEY_W))&&(i=-this.speed),(t.isPressed(pc.KEY_DOWN)||t.isPressed(pc.KEY_S))&&(i+=this.speed),this.force.set(s,0,i),this.force.lengthSq()>0){this.force.normalize().scale(this.speed);const e=.25*-Math.PI,t=Math.cos(e),s=Math.sin(e),i=this.force.x*t-this.force.z*s,o=this.force.z*t+this.force.x*s;this.force.set(i,0,o)}this.entity.rigidbody.applyImpulse(this.force)},Movement.prototype.teleport=function(e){this.entity.rigidbody.teleport(e),this.spawnPos.copy(e),this.entity.rigidbody.linearVelocity=pc.Vec3.ZERO,this.entity.rigidbody.angularVelocity=pc.Vec3.ZERO};var Teleporter=pc.createScript("teleporter");Teleporter.attributes.add("target",{type:"entity",title:"Target Entity",description:"The target entity where we are going to teleport"}),Teleporter.prototype.initialize=function(){const onTriggerEnter=t=>{if(t.script.movement){const e=this.target.getPosition().clone();e.y+=.5,t.script.movement.teleport(e)}};this.entity.collision.on("triggerenter",onTriggerEnter),this.on("destroy",(()=>{this.entity.collision.off("triggerenter",onTriggerEnter)}))};
+var FollowCamera = pc.createScript("followCamera");
+FollowCamera.attributes.add("target", { type: "entity", title: "Target" });
+FollowCamera.attributes.add("distance", {
+  type: "number",
+  default: 4,
+  title: "Distance",
+});
+
+FollowCamera.prototype.initialize = function () {
+  this.pos = new pc.Vec3();
+  this.offset = new pc.Vec3();
+};
+
+FollowCamera.prototype.postUpdate = function (t) {
+  if (this.target) {
+    this.offset.set(0.75, 1, 0.75).scale(this.distance);
+    this.pos.copy(this.target.getPosition()).add(this.offset);
+    this.pos.lerp(this.entity.getPosition(), this.pos, 0.1);
+    this.entity.setPosition(this.pos);
+  }
+};
+
+var Movement = pc.createScript("movement");
+Movement.attributes.add("speed", {
+  type: "number",
+  default: 0.1,
+  min: 0.05,
+  max: 0.5,
+  precision: 2,
+});
+
+Movement.prototype.initialize = function () {
+  this.force = new pc.Vec3();
+  this.spawnPos = this.entity.getPosition().clone();
+};
+
+Movement.prototype.update = function (e) {
+  if (this.entity.getPosition().y < -1) return this.teleport(this.spawnPos);
+
+  const kb = this.app.keyboard;
+  let s = 0;
+  let i = 0;
+
+  // Keyboard controls
+  if (kb.isPressed(pc.KEY_LEFT) || kb.isPressed(pc.KEY_A)) s = -this.speed;
+  if (kb.isPressed(pc.KEY_RIGHT) || kb.isPressed(pc.KEY_D)) s += this.speed;
+  if (kb.isPressed(pc.KEY_UP) || kb.isPressed(pc.KEY_W)) i = -this.speed;
+  if (kb.isPressed(pc.KEY_DOWN) || kb.isPressed(pc.KEY_S)) i += this.speed;
+
+  // Touch controls
+  const touches = this.app.touch ? this.app.touch.touches : [];
+  if (touches.length > 0) {
+    const touch = touches[0];
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    if (touch.x < w * 0.3) s = -this.speed;
+    else if (touch.x > w * 0.7) s = this.speed;
+    if (touch.y < h * 0.3) i = -this.speed;
+    else if (touch.y > h * 0.7) i = this.speed;
+  }
+
+  this.force.set(s, 0, i);
+
+  if (this.force.lengthSq() > 0) {
+    this.force.normalize().scale(this.speed);
+    // Rotate force to align with the camera angle
+    const angle = 0.25 * -Math.PI;
+    const cos = Math.cos(angle);
+    const sin = Math.sin(angle);
+    const rotatedX = this.force.x * cos - this.force.z * sin;
+    const rotatedZ = this.force.z * cos + this.force.x * sin;
+    this.force.set(rotatedX, 0, rotatedZ);
+
+    this.entity.rigidbody.applyImpulse(this.force);
+  }
+};
+
+Movement.prototype.teleport = function (pos) {
+  this.entity.rigidbody.teleport(pos);
+  this.entity.rigidbody.linearVelocity = pc.Vec3.ZERO;
+  this.entity.rigidbody.angularVelocity = pc.Vec3.ZERO;
+};
+
+var Teleporter = pc.createScript("teleporter");
+Teleporter.attributes.add("target", { type: "entity", title: "Target Entity" });
+
+Teleporter.prototype.initialize = function () {
+  const onTriggerEnter = (result) => {
+    if (result.script && result.script.movement) {
+      const p = this.target.getPosition().clone();
+      p.y += 0.5;
+      result.script.movement.teleport(p);
+    }
+  };
+  this.entity.collision.on("triggerenter", onTriggerEnter);
+};
