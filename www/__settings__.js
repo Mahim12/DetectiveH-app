7c8c89ab982f8f81a8bb3ea7d1a20775f86262c7
Mahim Dhungel <mdrezark@gmail.com>
2026-05-14 17:57:59 -0400

    Clean setup for PlayCanvas 3D build

---
+++ www/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2499254.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 290214371, 290214372, 290214370 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/290214389/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/290214393/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/290214384/1/ammo.js', 'preload' : true},
+];
